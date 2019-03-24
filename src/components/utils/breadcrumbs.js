c71fd26816a4a95d1768d94d9ac950429216d2bb
reivhax <xavierkibet@gmail.com>
2019-03-24 20:54:14 +0300

    update breadcrumbs

--- src/components/utils/breadcrumbs.js
+++ src/components/utils/breadcrumbs.js
@@ -5,9 +5,10 @@ import Route from "react-router-dom/es/Route";
 const BreadCrumbs = function(match){
   let url = match.match.params.path,
       full = match.match.url,
-      text = url.split('/')[0];
+      text = url.split('/')[0],
+      link = !match.match.isExact;
   return <span>
-     /&nbsp;<a href={full+'/'}>{text}</a>&nbsp;
+     /&nbsp;{link?<a href={full+'/'}>{text}</a>:<span className='text-dark'>{text}</span>}&nbsp;
     <Route path={`${full}/:path`} component={BreadCrumbs} />
   </span>
 };
