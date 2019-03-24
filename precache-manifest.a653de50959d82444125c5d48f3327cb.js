b035902504061acc925f2015bf69143653a8d644
reivhax <xavierkibet@gmail.com>
2019-03-25 01:16:35 +0300

    Updates

---
+++ precache-manifest.a653de50959d82444125c5d48f3327cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "288ac7de17071a5539e9",
+    "url": "/react-redux-github-search/static/css/main.dc24f425.chunk.css"
+  },
+  {
+    "revision": "288ac7de17071a5539e9",
+    "url": "/react-redux-github-search/static/js/main.6387fa55.chunk.js"
+  },
+  {
+    "revision": "203df01927ae9aa262cd",
+    "url": "/react-redux-github-search/static/js/runtime~main.ca9c0eab.js"
+  },
+  {
+    "revision": "f3cc6efbb57b67027d81",
+    "url": "/react-redux-github-search/static/css/2.6cabf53e.chunk.css"
+  },
+  {
+    "revision": "f3cc6efbb57b67027d81",
+    "url": "/react-redux-github-search/static/js/2.300d8195.chunk.js"
+  },
+  {
+    "revision": "e27ea24915b33fee2627063349999883",
+    "url": "/react-redux-github-search/index.html"
+  }
+];
